a195a07574910d24ad3203c5202a23a7e8374b5b
roman-shhh <55157344+roman-shhh@users.noreply.github.co>
2025-08-19 19:25:28 +0200

    small bugs fixed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -18,7 +18,7 @@ function SignUpPage() {
   }
 
   return (
-    <div className='h-full w-full hero-bg'>
+    <div className='h-screen w-full hero-bg'>
       <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
         <Link to="/">
           <img src='/netflix-logo.png' alt='logo' className='w-52' />
